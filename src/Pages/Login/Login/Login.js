56173627879048c8cfc90e9cda2d8734d05aa6db
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-05-17 23:24:02 +0600

    register form created

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,22 +1,34 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
     console.log(email, password);
   };
+
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
+
   return (
     <div className="container w-50 mx-auto">
       <h2 className="text-primary text-center mt-2">Please Login</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -28,6 +40,7 @@ const Login = () => {
             ref={passwordRef}
             type="password"
             placeholder="Password"
+            required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -37,6 +50,16 @@ const Login = () => {
           Submit
         </Button>
       </Form>
+      <p>
+        New to this Website{" "}
+        <Link
+          to="/register"
+          className="text-danger pe-auto text-decoration-none"
+          onClick={navigateRegister}
+        >
+          Please Register
+        </Link>
+      </p>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./Register.css";
 
 const Register = () => {
   return (
-    <div>
+    <div className="register-form">
       <h2>Please Register</h2>
+      <form>
+        <input type="text" name="name" id="" placeholder="Name" required />
+        <input type="email" name="email" id="" placeholder="Email" required />
+
+        <input
+          type="password"
+          name="password"
+          id=""
+          placeholder="Password"
+          required
+        />
+      </form>
     </div>
   );
 };
