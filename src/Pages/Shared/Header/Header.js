950da1092ed125905cc2214634e3ef983f4c22e8
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-05-17 11:33:02 +0600

    navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
   return (
     <header>
-      <h2>This is header</h2>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} height="30px" alt="" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#services">Service</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </header>
   );
 };
